11052e93a4d88a8cbf78116c91fd7f0445e54cbd
Tajuldin <A.Othman05768@student.aast.edu>
2025-04-19 21:32:02 +0200

    message update and servlet

--- src/main/java/aammo/ppv/HelloServlet.java
+++ src/main/java/aammo/ppv/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Habashhhhyyyyyy";
+        message = "Taj";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
