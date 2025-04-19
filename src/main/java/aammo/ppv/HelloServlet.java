6872bc9f7f89b964429cac8a56d65e774a6bab46
AbdulRahman-MFarag <A.Mostafa15031@student.aast.edu>
2025-04-19 21:12:11 +0200

    1st commit

--- src/main/java/aammo/ppv/HelloServlet.java
+++ src/main/java/aammo/ppv/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "BOOMBOOMBOOM AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+        message = "Habashy";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
