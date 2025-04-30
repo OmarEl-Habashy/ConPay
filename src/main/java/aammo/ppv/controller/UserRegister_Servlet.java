3510d63397ab998e8b9cea452fd62e10904fa400
Omar Emad El-Habashy <O.El-Haba06695@student.aast.edu>
2025-04-30 20:02:07 +0300

    Register added (not completed)

---
+++ src/main/java/aammo/ppv/controller/UserRegister_Servlet.java
@@ -0,0 +1,17 @@
+package aammo.ppv.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/register")
+public class UserRegister_Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/webapp/view/register.jsp").forward(request, response);
+    }
+}
